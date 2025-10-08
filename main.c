1a135cf1ddb4347799d235dee0c951d862fb3cf1
Emery <emerypakula@gmail.com>
2025-10-08 13:49:02 +0200

    etape 3 : recupérer les infos

--- main.c
+++ main.c
@@ -6,5 +6,11 @@ int main()
 	int number1;
 	int number2;
 	char symbol;
+	printf("Bonjour ! Veuillez taper un opérateur parmi les suivants : + - * / %%\n");
+	scanf("%c", &symbol);
+	printf("Veuillez taper le premier nombre :\n");
+	scanf("%d", &number1);
+	printf("Veuillez taper le second nombre :\n");
+	scanf("%d", &number2);
 	exit(0);
 }
