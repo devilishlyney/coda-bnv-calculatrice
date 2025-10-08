2ac2f498c73a1920fbff22087cb2bf02c499baee
Emery <emerypakula@gmail.com>
2025-10-08 12:03:49 +0000

    etape 4 : créer les conditions

--- main.c
+++ main.c
@@ -8,6 +8,30 @@ int main()
 	char symbol;
 	printf("Bonjour ! Veuillez taper un opérateur parmi les suivants : + - * / %%\n");
 	scanf("%c", &symbol);
+	if(symbol == 43)
+	{
+		printf("Addition\n");
+	}
+	else if(symbol == 42)
+	{
+		printf("Multiplication\n");
+	}
+	else if(symbol == 45)
+	{
+		printf("Soustraction\n");
+	}
+	else if(symbol == 47)
+	{
+		printf("Division\n");
+	}
+	else if(symbol == 37)
+	{
+		printf("Modulo\n");
+	}
+	else
+	{
+		printf("L'opération %c n'existe pas\n", symbol);
+	}
 	printf("Veuillez taper le premier nombre :\n");
 	scanf("%d", &number1);
 	printf("Veuillez taper le second nombre :\n");
