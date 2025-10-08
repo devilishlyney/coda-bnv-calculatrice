47b20de41b1c8487deeb5f129b80f81fc19df348
Emery <emerypakula@gmail.com>
2025-10-08 12:34:38 +0000

    etape 5 : l'opération

--- main.c
+++ main.c
@@ -6,35 +6,50 @@ int main()
 	int number1;
 	int number2;
 	char symbol;
+	int resultat;
 	printf("Bonjour ! Veuillez taper un opérateur parmi les suivants : + - * / %%\n");
 	scanf("%c", &symbol);
+	printf("Veuillez taper le premier nombre :\n");
+	scanf("%d", &number1);
+	printf("Veuillez taper le second nombre :\n");
+	scanf("%d", &number2);
 	if(symbol == 43)
 	{
-		printf("Addition\n");
+		int resultat = number1 + number2;
 	}
 	else if(symbol == 42)
 	{
-		printf("Multiplication\n");
+		int resultat = number1 * number2;
 	}
 	else if(symbol == 45)
 	{
-		printf("Soustraction\n");
+		int resultat = number1 - number2;
 	}
 	else if(symbol == 47)
 	{
-		printf("Division\n");
+		if(number2 == 0)
+		{
+			printf("t pas le couteau le plus croquant du paquet")
+		}
+		else
+		{
+		double resultat = (double)number1 / (double)number2;
+		}
 	}
 	else if(symbol == 37)
 	{
-		printf("Modulo\n");
+		if(number2 == 0)
+		{
+		printf("t pas le pingouin le plus aiguisé du tiroir")
+		}
+		else
+		{
+		int resultat = number1 % number2;
+		}
 	}
 	else
 	{
 		printf("L'opération %c n'existe pas\n", symbol);
 	}
-	printf("Veuillez taper le premier nombre :\n");
-	scanf("%d", &number1);
-	printf("Veuillez taper le second nombre :\n");
-	scanf("%d", &number2);
 	exit(0);
 }
